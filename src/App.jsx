eedb54b1e48d0a6845e70f5d99f2b0a68ad9973c
dev-rjw <rjw0105@gmail.com>
2024-08-13 19:18:59 +0900

    아무것도 없을 경우 '아직 추가된 국가가 없습니다. 메달을 추적하세요' 값이 나오도록 수정

--- src/App.jsx
+++ src/App.jsx
@@ -94,15 +94,19 @@ function App() {
             </form>
             <div>
                 <table>
-                    <thead>
-                        <tr>
-                            <th>국가명</th>
-                            <th>금메달</th>
-                            <th>은메달</th>
-                            <th>동메달</th>
-                            <th>액션</th>
-                        </tr>
-                    </thead>
+                    {countries.length !== 0 ? (
+                        <thead>
+                            <tr>
+                                <th>국가명</th>
+                                <th>금메달</th>
+                                <th>은메달</th>
+                                <th>동메달</th>
+                                <th>액션</th>
+                            </tr>
+                        </thead>
+                    ) : (
+                        "아직 추가된 국가가 없습니다. 메달을 추적하세요!"
+                    )}
                     <tbody>
                         {countries
                             .sort((a, b) => b.bronze - a.bronze)
